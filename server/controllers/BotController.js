31973d71fd59d2c6f744557a86422eb434321c33
unknown <ivan0109m@gmail.com>
2019-06-15 09:36:05 +0300

    ...

--- server/controllers/BotController.js
+++ server/controllers/BotController.js
@@ -8,6 +8,10 @@ const bot = new TelegramBot(TOKEN, {
   polling: true
 });
 
+bot.on('polling_error', (error) => {
+  //console.log('POLLING ERROR: телеграм заблокирован, включите VPN');  // error.code -> 'EFATAL'
+});
+
 
 module.exports.sendMessageToFelkerGroup = async (msg) => {
   return await bot.sendMessage(felkerChatID, msg, {
